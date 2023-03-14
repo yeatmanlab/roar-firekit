989a55f50847a378ed9a96e41ac52f48e96925e7
Mohammed Shoaib <shoaib98libra@gmail.com>
2023-03-14 15:18:31 -0700

    [functions] Add constants for the SWR look-up table

--- src/functions/config.ts
+++ src/functions/config.ts
@@ -1,3 +1,4 @@
-// TODO: remove this constant from firebase functions
+// TODO: check if we should be removing these constants from firebase functions
 export const FIREBASE_COLLECTIONS = ["external", "prod"];
-
+export const SWR_LOOKUP_TABLE_VERSION = 0.1;
+export const TASK_ID_SWR = "swr";
