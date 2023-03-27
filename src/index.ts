e6e16fba7f283be1b601d437ef27f45d62333189
Adam Richie-Halford <richiehalford@gmail.com>
2023-03-27 10:53:16 -0700

    Add WJPercentile to top-level exports

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+export { WJPercentile } from './functions/wj-percentile';
 export { RoarFirekit } from './firestore/firekit';
 export { RoarUser } from './firestore/user';
 export { RoarTaskVariant } from './firestore/task';
