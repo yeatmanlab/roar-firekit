f4491b5f7fa18ca3a19adf95d87080ccff06a41d
Adam Richie-Halford <richiehalford@gmail.com>
2023-04-21 12:42:43 -0700

    Move interfaces to interfaces.ts and add methods to read and write administrations and users

--- src/firestore/firekit.ts
+++ src/firestore/firekit.ts
@@ -1,9 +1,9 @@
-import { FirebaseApp } from 'firebase/app';
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+import _uniq from 'lodash/uniq';
+import _without from 'lodash/without';
 import {
-  Auth,
   AuthError,
   GoogleAuthProvider,
-  User,
   createUserWithEmailAndPassword,
   getRedirectResult,
   signInWithEmailAndPassword,
@@ -12,8 +12,8 @@ import {
   signInWithCredential,
 } from 'firebase/auth';
 import {
-  DocumentData,
-  Firestore,
+  addDoc,
+  arrayRemove,
   arrayUnion,
   collection,
   doc,
@@ -22,44 +22,24 @@ import {
   query,
   updateDoc,
 } from 'firebase/firestore';
-import { FirebaseConfigData, initializeProjectFirekit } from './util';
+import { initializeProjectFirekit } from './util';
+import {
+  IAdministrationData,
+  IAssessmentData,
+  IFirekit,
+  IMyAdministrationData,
+  IMyAssessmentData,
+  IRoarConfigData,
+  IUserData,
+  UserType,
+} from './interfaces';
 // import { ITaskVariantInput, RoarTaskVariant } from './task';
-
-interface IRoarConfigData {
-  app: FirebaseConfigData;
-  admin: FirebaseConfigData;
-}
-
-interface IFirekit {
-  firebaseApp: FirebaseApp;
-  db: Firestore;
-  auth: Auth;
-  user?: User;
-}
-
-enum supplementalDataType {
-  admin = 'adminData',
-  educator = 'educatorData',
-  student = 'studentData',
-  caregiver = 'caregiverData',
-}
-
-interface IRoarUserData extends DocumentData {
-  adminData?: DocumentData;
-  educatorData?: DocumentData;
-  studentData?: DocumentData;
-  caregiverData?: DocumentData;
-  // Allow for data from external resources like clever or state-wide tests
-  externalData?: {
-    [x: string]: unknown;
-  };
-}
 
 export class RoarFirekit {
   roarConfig: IRoarConfigData;
   app: IFirekit;
   admin: IFirekit;
-  userData?: IRoarUserData;
+  userData?: IUserData;
   /**
    * Create a RoarFirekit. This expects an object with keys `roarConfig`,
    * where `roarConfig` is a [[IRoarConfigData]] object.
@@ -170,43 +150,51 @@ export class RoarFirekit {
   // ----------|  End Authentication Methods  |----------
   //           +------------------------------+
 
-  async getUserAdminData() {
+  private _verify_authentication() {
     if (this.admin.user === undefined) {
       throw new Error('User is not authenticated.');
     }
+  }
 
-    const userDocRef = doc(this.admin.db, 'users', this.admin.user.uid);
+  private async _getUser(uid: string): Promise<IUserData | undefined> {
+    this._verify_authentication();
+    const userDocRef = doc(this.admin.db, 'users', uid);
     const userDocSnap = await getDoc(userDocRef);
 
     if (userDocSnap.exists()) {
-      this.userData = userDocSnap.data();
-      for (const dataType of Object.values(supplementalDataType)) {
-        const docRef = doc(userDocRef, dataType);
-        const docSnap = await getDoc(docRef);
-
-        if (docSnap.exists()) {
-          this.userData[dataType] = docSnap.data();
-        }
+      let userData = {} as IUserData;
+      if (userDocSnap.data().userType) {
+        userData = userDocSnap.data() as IUserData;
+      } else {
+        userData = {
+          userType: UserType.guest,
+          ...userDocSnap.data(),
+        };
       }
 
       const externalDataSnapshot = await getDocs(collection(userDocRef, 'externalData'));
       let externalData = {};
       externalDataSnapshot.forEach((doc) => {
+        // TODO: Elijah add externalData to the dummy data created by Python
         // doc.data() is never undefined for query doc snapshots returned by ``getDocs``
         externalData = {
           ...externalData,
           [doc.id]: doc.data(),
         };
       });
-      this.userData.externalData = externalData;
+      userData.externalData = externalData;
+
+      return userData;
     }
   }
 
-  async getUserAdminRoles() {
-    if (this.admin.user === undefined) {
-      throw new Error('User is not authenticated.');
-    }
+  async getMyData() {
+    this._verify_authentication();
+    this.userData = await this._getUser(this.admin.user!.uid);
+  }
 
+  async getMyAdminRoles() {
+    this._verify_authentication();
     const adminCollection = collection(this.app.db, 'admin');
     const q = query(adminCollection);
     const querySnapshot = await getDocs(q);
@@ -218,7 +206,7 @@ export class RoarFirekit {
     return roles;
   }
 
-  async addUserToAdminRequests() {
+  async addMeToAdminRequests() {
     const adminCollection = collection(this.app.db, 'admin');
     const requestsRef = doc(adminCollection, 'requests');
 
@@ -227,5 +215,217 @@ export class RoarFirekit {
     });
   }
 
+  // TODO: Adam write the appFirekit
   // createAppFirekit(taskInfo: ITaskVariantInput, rootDoc: string[]);
+
+  /* Return a list of all UIDs for users that this user has access to */
+  async listUsers() {
+    this._verify_authentication();
+    const adminUsersDoc = doc(this.admin.db, 'users', this.admin.user!.uid, 'adminData', 'users');
+    const docSnap = await getDoc(adminUsersDoc);
+    if (docSnap.exists()) {
+      return Object.keys(docSnap.data());
+    }
+    return null;
+  }
+
+  /* Return a list of Promises for user objects for each of the UIDs given in the input array */
+  getUsers(uidArray: string[]): Promise<IUserData | undefined>[] {
+    this._verify_authentication();
+    return uidArray.map((uid) => this._getUser(uid));
+  }
+
+  public get administrationsAssigned() {
+    return this.userData?.administrationsAssigned;
+  }
+
+  public get administrationsStarted() {
+    return this.userData?.administrationsStarted;
+  }
+
+  public get administrationsCompleted() {
+    return this.userData?.administrationsCompleted;
+  }
+
+  private async _getGlobalAdministration(administrationId: string): Promise<IAdministrationData | undefined> {
+    this._verify_authentication();
+    const docRef = doc(this.admin.db, 'administrations', administrationId);
+    const docSnap = await getDoc(docRef);
+
+    if (docSnap.exists()) {
+      return docSnap.data() as IAdministrationData;
+    }
+  }
+
+  getAdministrations(administrationIds: string[]): Promise<IAdministrationData | undefined>[] {
+    this._verify_authentication();
+    return administrationIds.map((id) => this._getGlobalAdministration(id));
+  }
+
+  private async _getMyAdministration(administrationId: string): Promise<IMyAdministrationData | undefined> {
+    this._verify_authentication();
+    const docRef = doc(this.admin.db, 'users', this.admin.user!.uid, 'administrations', administrationId);
+    const docSnap = await getDoc(docRef);
+
+    if (docSnap.exists()) {
+      return docSnap.data() as IMyAdministrationData;
+    }
+  }
+
+  getMyAdministrations(administrationIds: string[]): Promise<IMyAdministrationData | undefined>[] {
+    this._verify_authentication();
+    return administrationIds.map((id) => this._getMyAdministration(id));
+  }
+
+  async completeAdministration(administrationId: string) {
+    this._verify_authentication();
+    const docRef = doc(this.admin.db, 'users', this.admin.user!.uid, 'administrations', administrationId);
+    return updateDoc(docRef, { completed: true });
+  }
+
+  private async _updateAssessment(administrationId: string, taskId: string, updates: { [x: string]: unknown }) {
+    this._verify_authentication();
+    const docRef = doc(this.admin.db, 'users', this.admin.user!.uid, 'administrations', administrationId);
+    const docSnap = await getDoc(docRef);
+    if (docSnap.exists()) {
+      const assessmentInfo = docSnap.data().assessments.find((a: IMyAssessmentData) => a.taskId === taskId);
+      // First remove the old assessment to avoid duplication
+      await updateDoc(docRef, {
+        assessments: arrayRemove(assessmentInfo),
+      });
+      const newAssessmentInfo = {
+        ...assessmentInfo,
+        ...updates,
+      };
+      await updateDoc(docRef, {
+        assessments: arrayUnion(newAssessmentInfo),
+      });
+    }
+  }
+
+  async startAssessment(administrationId: string, taskId: string) {
+    this._verify_authentication();
+    return this._updateAssessment(administrationId, taskId, { startedOn: new Date() });
+  }
+
+  async showAssessmentReward(administrationId: string, taskId: string) {
+    this._verify_authentication();
+    return this._updateAssessment(administrationId, taskId, { rewardShown: true });
+  }
+
+  async completeAssessment(administrationId: string, taskId: string) {
+    await this._updateAssessment(administrationId, taskId, { completedOn: new Date() });
+
+    // Check to see if all of the assessments have been completed,
+    // If so, complete the administration
+    const docRef = doc(this.admin.db, 'users', this.admin.user!.uid, 'administrations', administrationId);
+    const docSnap = await getDoc(docRef);
+    if (docSnap.exists()) {
+      if (docSnap.data().assessments.every((a: IMyAssessmentData) => a.completedOn !== null)) {
+        this.completeAdministration(administrationId);
+      }
+    }
+  }
+
+  // These are all methods that will be important for admins, but not necessary for students
+  async createAdministration(assessments: IAssessmentData[], dateOpen: Date, dateClosed: Date, sequential = true) {
+    this._verify_authentication();
+
+    // First add the administration to the database
+    const administrationData: IAdministrationData = {
+      createdBy: this.admin.user!.uid,
+      assignedUsers: [],
+      assignedClasses: [],
+      assignedSchools: [],
+      assignedDistricts: [],
+      assignedGrades: [],
+      dateCreated: new Date(),
+      dateOpened: dateOpen,
+      dateClosed: dateClosed,
+      assessments: assessments,
+      sequential: sequential,
+    };
+    const administrationDocRef = await addDoc(collection(this.admin.db, 'administrations'), administrationData);
+
+    // Then add the ID to the admin's list of administrationsCreated
+    const userDocRef = doc(this.admin.db, 'users', this.admin.user!.uid);
+    await updateDoc(userDocRef, {
+      'adminData.administrationsCreated': arrayUnion(administrationDocRef.id),
+    });
+  }
+
+  async assignAdministrationToUsers(administrationId: string, userIds: string[]) {
+    this._verify_authentication();
+    const users = await Promise.all(this.getUsers(userIds));
+    const studentData = _without(
+      users.map((user: IUserData | undefined) => user?.studentData),
+      undefined,
+    );
+    const assignedClasses = _uniq(studentData.map((user) => user!.classId));
+    const assignedSchools = _uniq(studentData.map((user) => user!.schoolId));
+    const assignedDistricts = _uniq(studentData.map((user) => user!.districtId));
+    const assignedGrades = _uniq(studentData.map((user) => user!.grade));
+
+    const docRef = doc(this.admin.db, 'administrations', administrationId);
+    await updateDoc(docRef, {
+      assignedUsers: arrayUnion(userIds),
+      assignedClasses: arrayUnion(assignedClasses),
+      assignedSchools: arrayUnion(assignedSchools),
+      assignedDistricts: arrayUnion(assignedDistricts),
+      assignedGrades: arrayUnion(assignedGrades),
+    });
+  }
+
+  async unassignAdministrationToUsers(administrationId: string, userIds: string[]) {
+    this._verify_authentication();
+
+    const administrationInfo = await Promise.all(this.getAdministrations([administrationId]));
+    const currentlyAssignedUserIds = administrationInfo[0]?.assignedUsers;
+
+    // Remaining users that will survive this deletion
+    const remainingUserIds = _without(currentlyAssignedUserIds, ...userIds);
+    const remainingUsers = await Promise.all(this.getUsers(remainingUserIds));
+    const remainingUserData = _without(
+      remainingUsers.map((user: IUserData | undefined) => user?.studentData),
+      undefined,
+    );
+
+    // The users that will be deleted
+    const usersToRemove = await Promise.all(this.getUsers(userIds));
+    const userDataToRemove = _without(
+      usersToRemove.map((user: IUserData | undefined) => user?.studentData),
+      undefined,
+    );
+
+    // These are the org Ids that we need to keep even after removing the users
+    const classesToKeep = _uniq(remainingUserData.map((user) => user!.classId));
+    const schoolsToKeep = _uniq(remainingUserData.map((user) => user!.schoolId));
+    const districtsToKeep = _uniq(remainingUserData.map((user) => user!.districtId));
+    const gradesToKeep = _uniq(remainingUserData.map((user) => user!.grade));
+
+    // Initially, these are the org Ids for all of the users to be deleted
+    let classesToRemove = _uniq(userDataToRemove.map((user) => user!.classId));
+    let schoolsToRemove = _uniq(userDataToRemove.map((user) => user!.schoolId));
+    let districtsToRemove = _uniq(userDataToRemove.map((user) => user!.districtId));
+    let gradesToRemove = _uniq(userDataToRemove.map((user) => user!.grade));
+
+    // Now we remove the ones that we want to keep (from the remaining users)
+    // from the ones that we want to get rid of
+    classesToRemove = _without(classesToRemove, ...classesToKeep);
+    schoolsToRemove = _without(schoolsToRemove, ...schoolsToKeep);
+    districtsToRemove = _without(districtsToRemove, ...districtsToKeep);
+    gradesToRemove = _without(gradesToRemove, ...gradesToKeep);
+
+    const docRef = doc(this.admin.db, 'administrations', administrationId);
+    await updateDoc(docRef, {
+      assignedUsers: arrayRemove(userIds),
+      assignedClasses: arrayRemove(classesToRemove),
+      assignedSchools: arrayRemove(schoolsToRemove),
+      assignedDistricts: arrayRemove(districtsToRemove),
+      assignedGrades: arrayRemove(gradesToRemove),
+    });
+  }
+
+  // async createUser();
+  // async updateUser();
 }
---
+++ src/firestore/interfaces.ts
@@ -0,0 +1,122 @@
+import { FirebaseApp } from 'firebase/app';
+import { Auth, User } from 'firebase/auth';
+import { DocumentData, Firestore } from 'firebase/firestore';
+import { FirebaseConfigData } from './util';
+
+export interface IRoarConfigData {
+  app: FirebaseConfigData;
+  admin: FirebaseConfigData;
+}
+
+export interface IFirekit {
+  firebaseApp: FirebaseApp;
+  db: Firestore;
+  auth: Auth;
+  user?: User;
+}
+
+type Grade = number | 'K' | 'PK' | 'TK';
+
+export enum UserType {
+  admin = 'admin',
+  educator = 'educator',
+  student = 'student',
+  caregiver = 'caregiver',
+  guest = 'guest',
+}
+
+enum AdminLevel {
+  class = 'class',
+  school = 'school',
+  district = 'district',
+  study = 'study',
+}
+
+export interface IStudentOrEducatorData extends DocumentData {
+  classId: string;
+  previousClassIds: string[];
+  schoolId: string;
+  previousSchoolIds: string[];
+  districtId: string;
+  studies: string[];
+  previousStudies: string[];
+  previousDistrictIds: string[];
+  [x: string]: unknown;
+}
+
+export interface IStudentData extends IStudentOrEducatorData {
+  grade: Grade;
+}
+
+export interface IEducatorData extends IStudentOrEducatorData {
+  grades: Grade[];
+}
+
+export interface ICaregiverData extends DocumentData {
+  students: string[];
+  [x: string]: unknown;
+}
+
+export interface IAdminData extends DocumentData {
+  administrationsCreated: string[];
+  permissions: string[];
+  classes: string[];
+  studies: string[];
+  districts: string[];
+  schools: string[];
+  adminLevel: AdminLevel;
+  [x: string]: unknown;
+}
+
+export interface IUserData extends DocumentData {
+  userType: UserType;
+  assessmentPid?: string;
+  dob?: Date;
+  assessmentsCompleted?: string[];
+  assessmentsAssigned?: string[];
+  administrationsAssigned?: string[];
+  administrationsStarted?: string[];
+  administrationsCompleted?: string[];
+  adminData?: IAdminData;
+  educatorData?: IEducatorData;
+  studentData?: IStudentData;
+  caregiverData?: ICaregiverData;
+  // Allow for data from external resources like clever or state-wide tests
+  externalData?: {
+    [x: string]: unknown;
+  };
+}
+
+export interface IAssessmentData extends DocumentData {
+  taskId: string;
+  params: {
+    [x: string]: unknown;
+  };
+}
+
+export interface IAdministrationData extends DocumentData {
+  createdBy: string;
+  assignedUsers: string[];
+  assignedClasses: string[];
+  assignedSchools: string[];
+  assignedDistricts: string[];
+  assignedGrades: number[];
+  dateCreated: Date;
+  dateOpened: Date;
+  dateClosed: Date;
+  sequential: boolean;
+  assessments: IAssessmentData[];
+}
+
+export interface IMyAssessmentData extends DocumentData {
+  taskId: string;
+  runId: string | null;
+  completedOn: Date | null;
+  startedOn: Date | null;
+  rewardShown: boolean;
+}
+
+export interface IMyAdministrationData extends DocumentData {
+  completed: boolean;
+  assessments: IMyAssessmentData[];
+}
