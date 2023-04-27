db011890523037abf76ee69e0759875589a42c64
Mohammed Shoaib <shoaib98libra@gmail.com>
2023-04-27 10:47:52 -0700

    Update the SWR lookup table version from 0.1 to 0.2, coref issue-90 from roar-firebase-functions

--- src/functions/config.ts
+++ src/functions/config.ts
@@ -1,4 +1,4 @@
 export const FIREBASE_COLLECTIONS = ['external', 'prod'];
-export const SWR_LOOKUP_TABLE_VERSION = 0.1;
+export const SWR_LOOKUP_TABLE_VERSION = 0.2;
 export const TASK_ID_SWR = 'swr';
 export const YEATMAN_LAB_BUCKET = 'gse-yeatmanlab-export-csv';
