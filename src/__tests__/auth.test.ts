9814231f0e677bbc4aa9407a0ed544c7f90b82a9
Adam Richie-Halford <richiehalford@gmail.com>
2023-06-15 12:02:30 -0700

    Delete tests

--- src/__tests__/auth.test.ts
+++
@@ -1,8 +0,0 @@
-import { roarEmail } from '../auth';
-
-describe('roarEmail', () => {
-  it('returns a roar-auth email address', () => {
-    const pid = 'test-pid';
-    expect(roarEmail(pid)).toBe(`${pid}@roar-auth.com`);
-  });
-});
