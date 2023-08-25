767f10825e682f95d392921b1ad87c12e6e92702
Adam Richie-Halford <richiehalford@gmail.com>
2023-08-25 05:10:37 -0700

    Export the empty org utils

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ export { RoarFirekit } from './firestore/firekit';
 export { RoarAppkit } from './firestore/app/appkit';
 export { RoarAppUser } from './firestore/app/user';
 export { RoarTaskVariant } from './firestore/app/task';
-export { initializeFirebaseProject, getTreeTableOrgs } from './firestore/util';
+export { emptyOrg, emptyOrgList, getTreeTableOrgs, initializeFirebaseProject } from './firestore/util';
 export * from './firestore/query-admin';
 export * from './firestore/query-assessment';
