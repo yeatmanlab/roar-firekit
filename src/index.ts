b92388fd43783dc6de231c3c4fff48dc4b8fe27f
Adam Richie-Halford <richiehalford@gmail.com>
2023-11-20 15:41:08 -0800

    Remove query-admin from index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,4 @@ export { RoarAppkit } from './firestore/app/appkit';
 export { RoarAppUser } from './firestore/app/user';
 export { RoarTaskVariant } from './firestore/app/task';
 export { emptyOrg, emptyOrgList, getTreeTableOrgs, initializeFirebaseProject } from './firestore/util';
-export * from './firestore/query-admin';
 export * from './firestore/query-assessment';
