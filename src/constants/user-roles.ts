907a20b17820c4bc4a1fcae26e60034d217e59db
Lucas Song <lucas7song@gmail.com>
2025-03-25 21:43:05 +0100

    9.5.0

--- src/constants/user-roles.ts
+++ src/constants/user-roles.ts
@@ -1,7 +1,7 @@
 export enum UserRoles {
   SUPER_ADMIN = 'super_admin',
   PLATFORM_ADMIN = 'platform_admin',
-  LAUNCH_ADMIN= 'launch_admin',
+  LAUNCH_ADMIN = 'launch_admin',
   ADMIN = 'admin',
   STUDENT = 'student',
   GUEST = 'guest',
