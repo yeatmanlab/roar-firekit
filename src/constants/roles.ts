d81ec3a1ae394c0fbef79fd1188c121f3f8af8ed
Elijah Kelly <kellyel@stanford.edu>
2025-04-16 10:38:03 -0700

    Introduce CI Test Admin role

--- src/constants/roles.ts
+++ src/constants/roles.ts
@@ -52,6 +52,22 @@ export const roles = {
       Permissions.Tasks.LAUNCH,
     ],
   },
+  [UserRoles.CI_TEST_ADMIN]: {
+    title: 'CI/Test Admin',
+    permissions: [
+      Permissions.Administrators.READ,
+      Permissions.Administrators.CREATE,
+      Permissions.Administrators.UPDATE,
+      Permissions.Profile.ALL,
+      Permissions.Reports.Score.READ,
+      Permissions.Reports.Progress.READ,
+      Permissions.Reports.Student.READ,
+      Permissions.Organizations.ALL,
+      Permissions.Users.ALL,
+      Permissions.Tasks.LAUNCH,
+      Permissions.Administrations.ALL,
+    ],
+  },
   [UserRoles.SUPER_ADMIN]: {
     title: 'Super Admin',
     permissions: [],
