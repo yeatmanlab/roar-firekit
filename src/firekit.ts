3ba7fb7488e5383436031a357519b0d957d0d2e1
Emily Arteaga-Garcia <62304493+Emily-ejag@users.noreply.github.com>
2025-11-15 05:51:24 -0800

    Void email link and password reset for emailAvailable (#200)
    * prevent email creation and password reset errors
    
    * implementing copilot suggestions

--- src/firekit.ts
+++ src/firekit.ts
@@ -821,6 +821,11 @@ export class RoarFirekit {
    */
   async initiateLoginWithEmailLink({ email, redirectUrl }: { email: string; redirectUrl: string }) {
     this._verifyInit();
+    const emailAvailable = await isEmailAvailable(this.admin!.auth, email);
+    // If the email is available, we don't have a user with that email, but to prevent enumeration we mimic the behavior of sendSignInLinkToEmail
+    if (emailAvailable) {
+      return Promise.resolve(void 0);
+    }
     const actionCodeSettings = {
       url: redirectUrl,
       handleCodeInApp: true,
@@ -2244,6 +2249,12 @@ export class RoarFirekit {
    * @returns A promise that resolves when the password reset email is sent.
    */
   async sendPasswordResetEmail(email: string) {
+    this._verifyInit();
+    const emailAvailable = await isEmailAvailable(this.admin!.auth, email);
+    // If the email is available, we don't have a user with that email, but to prevent enumeration we mimic the behavior of sendPasswordResetEmail
+    if (emailAvailable) {
+      return Promise.resolve(void 0);
+    }
     return sendPasswordResetEmail(this.admin!.auth, email).then(() => {
       this.verboseLog('Password reset email sent to', email);
     });
